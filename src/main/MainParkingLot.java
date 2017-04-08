8a19a41f946cdf050710ce6cf575138ab5931e53
nandaz <superdiazzz@gmail.com>
2017-04-08 20:20:24 +0700

    third commit

--- src/main/MainParkingLot.java
+++ src/main/MainParkingLot.java
@@ -1,6 +1,11 @@
 package main;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+
+import model.Vihacle;
 
 
 public class MainParkingLot {
@@ -12,7 +17,54 @@ public class MainParkingLot {
 		System.out.println("create_parking_lot :");
 		int n = reader.nextInt();
 		
-		//System.out.println("your parking is "+n);
+		System.out.println("Created a parking lot with "+n+" slots");
+		
+		Map<Integer, Vihacle> map = new HashMap<Integer, Vihacle>();
+		
+		for(int k =0; k<n; k++){
+			String actWrite = reader.nextLine();
+			groupingVihacle(actWrite, map, k);	
+		}
+		
+		
+		String actWrite = reader.nextLine();
+		String action = getAction(actWrite);
+		if("PARK".equalsIgnoreCase(action)){
+			
+		}else if("LEAVE".equalsIgnoreCase(action)){
+			
+		}else if("STATUS".equalsIgnoreCase(action)){
+			showStatus(map);
+		}
+			
+		
+	}
+
+	private static void showStatus(Map<Integer, Vihacle> map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static String getAction(String actWrite) {
+		String[] str = actWrite.split("\\s");
+		String act = str[0];
+		String cleanAct = act.toUpperCase();
+		return cleanAct;
+	}
+
+	private static void groupingVihacle(String write, Map<Integer, Vihacle> map, int number) {
+		
+		String[] idVcRaw = write.split("\\s+");
+		String idVc = idVcRaw[0];
+		String col = idVcRaw[1];
+		
+		Vihacle vc = new Vihacle();
+		vc.setIdNumber(idVc);
+		vc.setColour(col);
+		vc.setNo(number);
+		
+		map.put(number, vc);
+		
 	}
 
 }
---
+++ src/model/Vihacle.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Vihacle {
+	
+	private int no;
+	private String idNumber;
+	private String colour;
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getIdNumber() {
+		return idNumber;
+	}
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	
+	
+
+}
