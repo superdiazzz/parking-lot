fde3823ef422ee1e0211e811562456b3d94e2431
nandaz <superdiazzz@gmail.com>
2017-04-08 19:28:46 +0700

    second commit

--- src/main/MainParkingLot.java
+++ src/main/MainParkingLot.java
@@ -1,11 +1,18 @@
 package main;
 
+import java.util.Scanner;
+
 
 public class MainParkingLot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello world, by nanda");
+			//System.out.println("Hello world, by nanda");
+		Scanner reader = new Scanner(System.in);
+		System.out.println("create_parking_lot :");
+		int n = reader.nextInt();
+		
+		//System.out.println("your parking is "+n);
 	}
 
 }
