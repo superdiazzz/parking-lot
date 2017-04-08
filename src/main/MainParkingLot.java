2c62c793335844544c64259ced5fb7337d542e27
nandaz <superdiazzz@gmail.com>
2017-04-08 12:52:46 +0700

    first commit

---
+++ src/main/MainParkingLot.java
@@ -0,0 +1,11 @@
+package main;
+
+
+public class MainParkingLot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello world, by nanda");
+	}
+
+}
